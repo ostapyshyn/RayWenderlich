e253c26e55e2f1bd8531a4f142b798eaf5e0a2a6
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-25 12:26:34 +0300

    add new project BullsEye

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Volodymyr Ostapyshyn on 25.04.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(_ sender: UIButton) {
+        print("Hello!")
+    }
+    
+}
+
