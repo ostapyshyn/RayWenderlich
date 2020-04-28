b85dac6440d9246120c8a1b07df1242076b09502
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-28 18:17:07 +0300

    finish part 5

--- BullsEye/BullsEye/AboutVC.swift
+++ BullsEye/BullsEye/AboutVC.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutVC: UIViewController {
-
+    
+    @IBOutlet var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
     
     @IBAction func touchClose(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-
+    
 }
